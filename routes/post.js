6e28150952daccb96b144dc742877accf63abb2f
andrew1123 <leechihoandrew1123@gmail.com>
2020-06-10 20:24:27 +0800

    post get and patch route

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const Post = require("../model/post");
 const User = require("../model/userDetail");
 
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 router.post("/", async (req, res) => {
   const { user, content } = req.body;
 
@@ -23,9 +33,28 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.patch("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    for (let field in req.body) {
+      post[field] = req.body[field];
+    }
+
+    const updatedPost = await post.save();
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   try {
-    const post = await Post.findOneById(req.params.id);
+    const post = await Post.findById(req.params.id);
     if (!post) {
       return res.status(404).json({ message: "Post not found" });
     }
